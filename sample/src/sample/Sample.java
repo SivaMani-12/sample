d13ce6637c0b212ba8914de48e6e114e5a1eb66a
Siva Mani <pokalasiva12@gmail.com>
2024-07-09 11:50:08 +0530

    Simple java file

---
+++ sample/src/sample/Sample.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Addition of two number");
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+	}
+
+}
